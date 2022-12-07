1cd4132272c8502c2c40ceefb59b509636436a1e
darks-society <anasmoiammor@gmail.com>
2022-12-07 10:29:30 +0100

    test ensure role [skip ci]

--- src/middlewares/ensure-role.middlewares.ts
+++ src/middlewares/ensure-role.middlewares.ts
@@ -1,15 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import { UnAuthorizedError } from '../errors/UnAuthorized.errors';
+import { ForbiddenError } from '../errors/Forbidden.errors';
 import RoleAttrs from '../interfaces/role.interfaces';
 import userServices from '../services/user.services';
 
-export const ensureRole = async (roles: RoleAttrs[]) => {
+export const ensureRole = async (role: RoleAttrs) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     let currentUser = req.currentUser;
     if (!currentUser) throw new UnAuthorizedError();
     let user = await userServices.getByEmail(currentUser);
     if (!user) throw new UnAuthorizedError();
     let userRoles = user.roles;
-    roles.map((role) => {});
+    const hasRole = userRoles.some((role) => {
+      role === role;
+    });
+    if (!hasRole) throw new ForbiddenError('Forbidden');
+    next();
   };
 };
