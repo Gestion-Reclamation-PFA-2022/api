2c47428f76ecf9ef4896fac4f6cd1d106e3e108a
darks-society <anasmoiammor@gmail.com>
2022-12-14 11:41:04 +0100

    refactoring [skip ci]

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,9 +8,9 @@ const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.MYSQL_DB_HOST || 'localhost',
   port: parseInt(process.env.MYSQL_DB_PORT as string) || 3306,
-  username: process.env.MYSQL_DB_USERNAME || 'root',
-  database: process.env.MYSQL_DB_NAME || "testdb",
-  password: process.env.MYSQL_DB_PASSWORD || '123',
+  username: process.env.MYSQL_DB_USERNAME,
+  database: process.env.MYSQL_DB_NAME,
+  //password: process.env.MYSQL_DB_PASSWORD || '123',
   entities: glob.sync(__dirname + '/../models/*.js'),
   synchronize: true,
 });
